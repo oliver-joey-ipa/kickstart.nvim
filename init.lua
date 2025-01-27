ac28aa61feef18778235be11080c5eb6549e0093
Joey Oliver <7379779+Oliver-Joey@users.noreply.github.com>
2025-01-26 20:13:14 -0500

    added markdown formatter

--- init.lua
+++ init.lua
@@ -641,6 +641,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         vtsls = {},
+        mdformat = {},
         markdownlint = {},
         pyright = {
           settings = {
