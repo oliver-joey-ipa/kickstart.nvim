b7f71991299f7901c584891fc5db4fa37f295ab1
Joey Oliver <joliver@ipaglobal.com>
2025-07-22 12:42:36 -0400

    added uv and other python files

--- init.lua
+++ init.lua
@@ -1077,6 +1077,9 @@ require('lazy').setup({
   require 'custom.plugins.fugitive',
   require 'custom.plugins.git-conflict',
   require 'custom.plugins.trouble',
+  require 'custom.plugins.uv',
+  -- require 'custom.plugins.jupytext',
+  -- require 'custom.plugins.molten',
   -- require 'custom.plugins.llm',
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
