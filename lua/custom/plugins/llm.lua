a4fae689f6555a208c068ec81ca399acf326994c
Joey Oliver <7379779+Oliver-Joey@users.noreply.github.com>
2025-11-30 21:43:19 -0500

    move to supermaven

--- lua/custom/plugins/llm.lua
+++ lua/custom/plugins/llm.lua
@@ -1,37 +1,23 @@
 return {
-  'yetone/avante.nvim',
-  event = 'VeryLazy',
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    provider = 'gemini',
-    providers = {
-      gemini = {
-        endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
-        model = 'gemini-2.0-flash-lite',
-        api_key_name = 'GEMINI_API_KEY',
+  'supermaven-inc/supermaven-nvim',
+  config = function()
+    require('supermaven-nvim').setup {
+      keymaps = {
+        accept_suggestion = '<Tab>',
+        clear_suggestion = '<C-]>',
+        accept_word = '<C-j>',
       },
-    },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = 'make',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
-    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { 'markdown', 'Avante' },
+      ignore_filetypes = {}, -- or { "cpp", }
+      color = {
+        suggestion_color = '#ffffff',
+        cterm = 244,
       },
-      ft = { 'markdown', 'Avante' },
-    },
-  },
+      log_level = 'info', -- set to "off" to disable logging completely
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+      disable_keymaps = false, -- disables built in keymaps for more manual control
+      condition = function()
+        return false
+      end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+    }
+  end,
 }
