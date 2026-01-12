37ee30791a3be1361d7bb07e056e284b1a0eb068
Joey Oliver <58233348+oliver-joey-ipa@users.noreply.github.com>
2026-01-12 10:33:37 -0500

    change colors for comments and line numbers

--- init.lua
+++ init.lua
@@ -760,11 +760,24 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      on_highlights = function(hl)
+        hl.LineNrAbove = {
+          fg = '#b5b8c2',
+        }
+        hl.LineNrBelow = {
+          fg = '#b5b8c2',
+        }
+        hl.Comment = {
+          fg = '#a9b8bf',
+        }
+      end,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
