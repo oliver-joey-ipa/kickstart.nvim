de6ef6a71788826317a53432e908704e45b7dfe1
Joey Oliver <58233348+oliver-joey-ipa@users.noreply.github.com>
2026-03-30 08:36:14 -0400

    update to packages and remove blink

--- init.lua
+++ init.lua
@@ -595,24 +595,6 @@ require('lazy').setup({
       }
     end,
   },
-  { -- optional blink completion source for require statements and module annotations
-    'saghen/blink.cmp',
-    version = '1.*',
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-        providers = {
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-    },
-  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
